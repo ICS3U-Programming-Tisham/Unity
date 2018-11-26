20fff693cdd81c8ff45ea70af7e66ad29e0d4134
Tisham <tisham.islam@ocsbstudent.ca>
2018-11-26 01:40:47 -0500

    Added Bolts

---
+++ SpaceShooterTisham/Assets/Scripts/Mover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Mover : MonoBehaviour 
+{
+
+	public float speed;
+	private Rigidbody rb;
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate()
+	{
+		rb.AddForce (0, 0, speed * Time.deltaTime);
+	}
+}
---
+++ SpaceShooterTisham/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary 
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+	private Rigidbody rb;
+	void Start()
+	 {
+		 rb = GetComponent<Rigidbody>();
+	 }
+
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
+    private float nextFire;
+	void Update()
+	{
+	 if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		}
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHor = Input.GetAxis ("Horizontal");
+		float moveVer = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHor, 0.0f, moveVer);
+		rb.velocity = movement * speed;
+		
+		rb.position = new Vector3
+		(
+			Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax), 
+			0.0f, 
+			Mathf.Clamp (rb.position.z, boundary.zMin, boundary.zMax)
+		);
+		
+		rb.rotation = Quaternion.Euler (rb.velocity.z * 0.5f * tilt, 0.0f, rb.velocity.x * -tilt);
+	}
+}
