dd92e0a742e57bc4a88a45041ae51fa59d404ca5
Tisham Islam <tisham.islam@ocsbstudent.ca>
2018-11-30 09:01:12 -0500

    Deleted extra unity folder, that was in there for some reason. Added new scene
    Survival Scene: 30 seconds, with "random" lasers coming in from the top, with more coming every

---
+++ Unity--Hopefully-finished/SpaceShooterTisham/Assets/Scripts/Mover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Mover : MonoBehaviour 
+{
+
+	public float speed;
+	private Rigidbody rb;
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate()
+	{
+		rb.velocity = new Vector3(0.0f, 0.0f, speed * Time.deltaTime);
+	}
+}
---
+++ Unity--Hopefully-finished/SpaceShooterTisham/Assets/Scripts/TurretMode.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretMode : MonoBehaviour
+{
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        rb.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+	}
+}
